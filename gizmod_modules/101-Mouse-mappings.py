4c02783d1c6287df508351467a5c203a11430b07
ohel <ohel@users.noreply.github.com>
2017-04-09 22:09:48 +0300

    Update mouse tweaks.

--- gizmod_modules/101-Mouse-mappings.py
+++ gizmod_modules/101-Mouse-mappings.py
@@ -17,31 +17,32 @@ class MouseMappings(GizmoScriptDefault):
         Called from Base Class' onEvent method.
         See GizmodDispatcher.onEvent documention for an explanation of this function
         """
-        if Event.Class == GizmoEventClass.Standard and Gizmo.FileName == MOUSE_DEVICE:
-            if Event.Type == GizmoEventType.EV_KEY:
-                if str(Event.Code) == "BTN_SIDE":
-                    if Event.Value == 1:
-                        Gizmod.Keyboards[0].createEventRaw(GizmoEventType.EV_KEY, GizmoKey.KEY_ENTER, 1)
-                    elif Event.Value == 0:
-                        Gizmod.Keyboards[0].createEventRaw(GizmoEventType.EV_KEY, GizmoKey.KEY_ENTER, 0)
-                elif str(Event.Code) == "BTN_EXTRA":
-                    if Event.Value == 1:
-                        Gizmod.Keyboards[0].createEventRaw(GizmoEventType.EV_KEY, GizmoKey.KEY_LEFTCTRL, 1)
-                    elif Event.Value == 0:
-                        Gizmod.Keyboards[0].createEventRaw(GizmoEventType.EV_KEY, GizmoKey.KEY_LEFTCTRL, 0)
-                elif str(Event.Code) == "BTN_MIDDLE":
-                    if Event.Value == 1:
-                        Gizmod.Keyboards[0].createEventRaw(GizmoEventType.EV_KEY, GizmoKey.KEY_ESC, 1)
-                    elif Event.Value == 0:
-                        Gizmod.Keyboards[0].createEventRaw(GizmoEventType.EV_KEY, GizmoKey.KEY_ESC, 0)
-            elif Event.Type == GizmoEventType.EV_REL and Event.RawCode == 6:
-                if Event.Value == 1:
-                    Gizmod.Keyboards[0].createEvent(GizmoEventType.EV_KEY, GizmoKey.KEY_LEFT)
-                elif Event.Value == -1:
-                    Gizmod.Keyboards[0].createEvent(GizmoEventType.EV_KEY, GizmoKey.KEY_RIGHT)
-            return True
-        else:
+        if Event.Class != GizmoEventClass.Standard or Gizmo.FileName != MOUSE_DEVICE:
             return False
+
+        if Event.Type == GizmoEventType.EV_KEY:
+            if str(Event.Code) == "BTN_SIDE":
+                if Event.Value == 1:
+                    Gizmod.Keyboards[0].createEventRaw(GizmoEventType.EV_KEY, GizmoKey.KEY_ENTER, 1)
+                elif Event.Value == 0:
+                    Gizmod.Keyboards[0].createEventRaw(GizmoEventType.EV_KEY, GizmoKey.KEY_ENTER, 0)
+            elif str(Event.Code) == "BTN_EXTRA":
+                if Event.Value == 1:
+                    Gizmod.Keyboards[0].createEventRaw(GizmoEventType.EV_KEY, GizmoKey.KEY_LEFTCTRL, 1)
+                elif Event.Value == 0:
+                    Gizmod.Keyboards[0].createEventRaw(GizmoEventType.EV_KEY, GizmoKey.KEY_LEFTCTRL, 0)
+            elif str(Event.Code) == "BTN_MIDDLE":
+                if Event.Value == 1:
+                    Gizmod.Keyboards[0].createEventRaw(GizmoEventType.EV_KEY, GizmoKey.KEY_ESC, 1)
+                elif Event.Value == 0:
+                    Gizmod.Keyboards[0].createEventRaw(GizmoEventType.EV_KEY, GizmoKey.KEY_ESC, 0)
+        elif Event.Type == GizmoEventType.EV_REL and Event.RawCode == 6:
+            if Event.Value == 1:
+                Gizmod.Keyboards[0].createEvent(GizmoEventType.EV_KEY, GizmoKey.KEY_LEFT)
+            elif Event.Value == -1:
+                Gizmod.Keyboards[0].createEvent(GizmoEventType.EV_KEY, GizmoKey.KEY_RIGHT)
+
+        return True
 
     def __init__(self):
         """
